8a624e9b334ff16a9670b3973c38daf046d26101
Valerator <93032554+Valerator@users.noreply.github.com>
2023-04-18 16:42:38 +0300

    Create 9. Palindrome Number.cpp

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,37 @@
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+  
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) //если число меньше 0, или оно заканчивается на ноль и не является 0, то оно не может быть палиндромом
+            return false;
+        long int ans = 0; //сюда записываем перевернутое число
+        int x_1 = x;
+        while(x > 0) {
+            ans = ans * 10 + x % 10;
+            x = x / 10;
+        }
+        return ans == x_1;
+    }
+};
